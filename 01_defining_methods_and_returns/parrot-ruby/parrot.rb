a88ef9c2d7448a8af2f71eadc0125661de4b78e9
brewchetta <chett.tiller@gmail.com>
2020-06-16 19:46:41 -0400

    added bonus questions

--- 01_defining_methods_and_returns/parrot-ruby/parrot.rb
+++ 01_defining_methods_and_returns/parrot-ruby/parrot.rb
@@ -4,3 +4,26 @@ def parrot(the_thing_the_parrot_says="Squawk!")
   puts the_thing_the_parrot_says
   return the_thing_the_parrot_says
 end
+
+# bonus questions:
+# what will be returned by the following code?
+
+def parrot_two
+
+  return "Squawk!"
+
+  return "Squawkeroni!"
+
+  return "Squawkity squawk squawk!"
+
+end
+
+def parrot_three
+
+  return "Squawky-doo"
+
+  puts "I'm after a return statement, will I appear?"
+
+  "Squawky-doo"
+
+end
