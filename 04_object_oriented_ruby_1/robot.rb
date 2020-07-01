d61eb38c30af13c402fd85f5605a65c33e8a376b
brewchetta <chett.tiller@gmail.com>
2020-07-01 17:37:03 -0400

    Minor changes

--- 04_object_oriented_ruby_1/robot.rb
+++ 04_object_oriented_ruby_1/robot.rb
@@ -2,21 +2,21 @@ require "pry"
 
 class Robot
 
-  attr_readers(:name, :special_feature) # creates a getter / reader
-  attr_writers :name, :special_feature  # creates a setter / writer
+  attr_reader(:special_feature) # creates a getter / reader
+  attr_writer :special_feature  # creates a setter / writer
 
-  # def name # getter / reader
-  #   @name
-  # end
+  def name # getter / reader
+    @name
+  end
 
-  # def name=(name_arg) # setter / writer
-  #   @name = name_arg
-  # end
+  def name=(name_arg) # setter / writer
+    @name = name_arg
+  end
 
   def initialize(name, special_feature)
     # @ denotes an instance variable
     @name = name
-    @features = features
+    @special_feature = special_feature
     # we can call other instance methods inside initialize if we want
     activate_robot
   end
@@ -25,13 +25,12 @@ class Robot
     puts "Beep boop my designation is: #{@name}"
   end
 
-
   private
+  # a private method can only be called from within the class, not through dot notation
 
   def activate_robot
     puts "#{name} is powering up..."
   end
-  # a private method can only be called from within the class, not through dot notation
 
 end # end Robot class
 
