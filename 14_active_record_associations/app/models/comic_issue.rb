8654db81cad7954a0c479fad681ec66b903176ab
brewchetta <chett.tiller@gmail.com>
2020-08-27 19:21:10 -0400

    Added dependent destroy

--- 14_active_record_associations/app/models/comic_issue.rb
+++ 14_active_record_associations/app/models/comic_issue.rb
@@ -1,4 +1,4 @@
 class ComicIssue < ActiveRecord::Base
-  has_many :hero_issues
+  has_many :hero_issues, dependent: :destroy
   has_many :heros, through: :hero_issues
 end
--- 14_active_record_associations/app/models/hero.rb
+++ 14_active_record_associations/app/models/hero.rb
@@ -1,4 +1,4 @@
 class Hero < ActiveRecord::Base
-  has_many :hero_issues
+  has_many :hero_issues, dependent: :destroy
   has_many :comic_issues, through: :hero_issues
 end
