f151031bcfecb3bc9dfcecd54082eb2469b4ddf6
brewchetta <chett.tiller@gmail.com>
2020-09-15 17:17:44 -0400

    Moved signup first

--- 18_user_auth/app/controllers/users_controller.rb
+++ 18_user_auth/app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
 
-  get "/login" do
-    erb :"users/login"
-  end
-
   get "/signup" do
     erb :"users/signup"
+  end
+
+  get "/login" do
+    erb :"users/login"
   end
 
 end
