ee16c7f62a8a19721f223cc36a58325b6ef183bb
brewchetta <chett.tiller@gmail.com>
2020-10-20 16:08:08 -0400

    Various

--- 21-rails-crud/scary-movies/app/controllers/scary_movies_controller.rb
+++ 21-rails-crud/scary-movies/app/controllers/scary_movies_controller.rb
@@ -13,18 +13,25 @@ class ScaryMoviesController < ApplicationController
   end
 
   def create
-    @scary_movie = ScaryMovie.new(params)
+    @scary_movie = ScaryMovie.new(scary_movie_params)
     if @scary_movie.save
-      redirect_to scary_movie_path(@movie)
+      redirect_to scary_movie_path(@scary_movie)
     else
       render :new
     end
   end
 
   def edit
+    find_movie
   end
 
   def update
+    find_movie
+    if @scary_movie.update(scary_movie_params)
+      redirect_to scary_movie_path(@scary_movie)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -34,6 +41,10 @@ class ScaryMoviesController < ApplicationController
 
   def find_movie
     @scary_movie = ScaryMovie.find_by(id: params[:id])
+  end
+
+  def scary_movie_params
+    params.require(:scary_movie).permit(:title, :scariness_rating)
   end
 
 end
