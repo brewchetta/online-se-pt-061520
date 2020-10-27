b7640ca7ee438cb1e5c282a175dae8236b0738fb
brewchetta <chett.tiller@gmail.com>
2020-10-27 18:55:13 -0400

    22 scary movie validations

--- 22-rails-validations/scary-movies/app/controllers/scary_movies_controller.rb
+++ 22-rails-validations/scary-movies/app/controllers/scary_movies_controller.rb
@@ -12,8 +12,10 @@ class ScaryMoviesController < ApplicationController
   def create
     @scary_movie = ScaryMovie.new(scary_movie_params)
     if @scary_movie.save
+      flash[:messages] = ["#{@scary_movie.title} was successfully created"]
       redirect_to scary_movie_path(@scary_movie)
     else
+      flash[:messages] = @scary_movie.errors.full_messages
       redirect_to new_scary_movie_path
     end
   end
@@ -39,7 +41,7 @@ class ScaryMoviesController < ApplicationController
   end
 
   def scary_movie_params
-    params.require(:scary_movie).permit(:title, :scariness_rating)
+    params.require(:scary_movie).permit(:title, :scariness_rating, :title_confirmation)
   end
 
 end
--- 22-rails-validations/scary-movies/app/models/scary_movie.rb
+++ 22-rails-validations/scary-movies/app/models/scary_movie.rb
@@ -1,7 +1,18 @@
 class ScaryMovie < ApplicationRecord
 
-  validates :title, presence: :true, uniqueness: :true
+  validates :title, presence: :true, uniqueness: :true, length: { minimum: 2, maximum: 200}, confirmation: true
 
-  validates :scariness_rating, presence: :true
+  validates :title_confirmation, presence: true
+
+  validates :scariness_rating, presence: :true, numericality: { only_integer: true, greater_than: 0, less_than: 11 }
+
+  validate :no_cuss_words
+
+  def no_cuss_words
+    if self.title.include?("Heck")
+      errors.add(:title, "no cuss words please")
+    end
+  end
+
 
 end
