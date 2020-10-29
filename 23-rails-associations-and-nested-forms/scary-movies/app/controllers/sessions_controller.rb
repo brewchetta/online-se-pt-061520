45e8062104fa1cbf6cca25f6c5438cba784dfe7d
brewchetta <chett.tiller@gmail.com>
2020-10-29 17:28:35 -0400

    minor tweaks

--- 23-rails-associations-and-nested-forms/scary-movies/app/controllers/sessions_controller.rb
+++ 23-rails-associations-and-nested-forms/scary-movies/app/controllers/sessions_controller.rb
@@ -10,7 +10,6 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      flash[:errors] = ["Invalid email or password"]
       redirect_back fallback_location: login_path
     end
   end
--- 23-rails-associations-and-nested-forms/scary-movies/db/schema.rb
+++ 23-rails-associations-and-nested-forms/scary-movies/db/schema.rb
@@ -22,9 +22,9 @@ ActiveRecord::Schema.define(version: 2020_10_23_185016) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "username"
   end
 
 end
