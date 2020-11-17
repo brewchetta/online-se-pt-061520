701bbf0061214bccc44e087572897f080dba847c
brewchetta <chett.tiller@gmail.com>
2020-11-17 16:45:03 -0500

    added user id to review, migrated

--- 26-omniauth/boardgamer/db/schema.rb
+++ 26-omniauth/boardgamer/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_193948) do
+ActiveRecord::Schema.define(version: 2020_11_17_214317) do
 
   create_table "boardgame_categories", force: :cascade do |t|
     t.integer "boardgame_id"
@@ -38,6 +38,15 @@ ActiveRecord::Schema.define(version: 2020_11_06_193948) do
   create_table "reviews", force: :cascade do |t|
     t.string "content"
     t.integer "boardgame_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
