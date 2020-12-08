c37228958bf1766f4d096ef7d9088dfcfc5199c6
brewchetta <chett.tiller@gmail.com>
2020-12-08 17:02:07 -0500

    styling for css and js

--- 27-intro-to-the-dom/app/index.js
+++ 27-intro-to-the-dom/app/index.js
@@ -1,2 +1,2 @@
-console.log("Welcome to the console!")
-console.log("Let's find a few elements on the DOM...")
+console.log(`%cWelcome to the console!`, "color: blue; font-size: 20px;")
+console.log(`Let's find a few elements on the DOM...`)
