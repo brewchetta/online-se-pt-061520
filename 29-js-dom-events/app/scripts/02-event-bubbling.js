36e250ffbd67921afea3a08d2ae91d1d67bf0810
brewchetta <chett.tiller@gmail.com>
2020-12-15 16:51:34 -0500

    various for pre-lecture prep

---
+++ 29-js-dom-events/app/scripts/02-event-bubbling.js
@@ -0,0 +1,25 @@
+const bubblingDiv = document.getElementById('bubbling-div')
+const outerDiv = document.getElementById('outer-div')
+const middleDiv = document.getElementById('middle-div')
+const innerDiv = document.getElementById('inner-div')
+
+seeBubblingAndCapturing(bubblingDiv)
+
+function seeBubblingAndCapturing(parentElement) {
+  for(let elem of parentElement.querySelectorAll('*')) {
+
+    elem.addEventListener("click",
+      function(event) {
+        this.style.backgroundColor = 'blue'
+        alert(`Capturing: ${elem.id}\n event.target: ${event.target.id}`)
+      },
+    true)
+
+    elem.addEventListener("click",
+      function(event) {
+        this.style.backgroundColor = 'white'
+        alert(`Bubbling: ${elem.id}\n event.target: ${event.target.id}`)
+      }
+    );
+  }
+}
