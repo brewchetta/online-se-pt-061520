c03746138c7812d5e5f10f24f6f7ba5e06152e96
brewchetta <chett.tiller@gmail.com>
2020-12-22 14:23:45 -0500

    moving cat no longer scrolls up and down

--- 30-js-fetch/app/scripts/cat-movement-handler.js
+++ 30-js-fetch/app/scripts/cat-movement-handler.js
@@ -2,25 +2,29 @@ const cat = document.querySelector('#cat-square')
 let fromRight = 100
 let fromTop = 100
 
-document.addEventListener("keyup", function(event) {
+document.addEventListener("keydown", function(event) {
   // console.log(event)
   switch (event.keyCode) {
     case 38:
+      event.preventDefault()
       // goes up
       fromTop -= 10
       cat.style.top = fromTop + "px"
       break;
     case 40:
+      event.preventDefault()
       // goes down
       fromTop += 10
       cat.style.top = fromTop + "px"
       break;
     case 37:
+      event.preventDefault()
       // goes left
       fromRight += 10
       cat.style.right = fromRight + "px"
       break;
     case 39:
+      event.preventDefault()
       // goes right
       fromRight -= 10
       cat.style.right = fromRight + "px"
