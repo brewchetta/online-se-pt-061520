ed94db158094a9481db46b8b42f74e5190b37f97
brewchetta <chett.tiller@gmail.com>
2020-12-28 16:01:00 -0500

    all the cats for 34 - js classes

--- 34-object-oriented-js/index.js
+++ 34-object-oriented-js/index.js
@@ -1,3 +1,46 @@
 class Animal {
-  // bring the ruckus
+
+  constructor(name, canBePet, diet) {
+    this.name = name
+    this.canBePet = canBePet
+    this.diet = diet
+  }
+
+  makeSound = function() {
+    console.log(`${this.name} made a sound`)
+  }
+
 }
+
+class Cat extends Animal {
+
+  constructor(name, canBePet, diet, livesRemaining) {
+    super(name, canBePet, diet)
+    this.livesRemaining = livesRemaining
+    this.constructor.allCats.push(this)
+  }
+
+  static allCats = []
+
+  static sayAllCatNames = () => {
+    this.allCats.forEach(cat => console.log(cat.name))
+  }
+
+  takeBath = () => {
+    return `${this.name} licks their fur`
+  }
+
+  makeSound = () => {
+    console.log(`${this.name} meowed`)
+
+    const whatIsThis = () => console.log("This is: ", this)
+
+    whatIsThis()
+  }
+
+}
+
+
+octavia = new Cat("Octavia", true, "fish as in now", 7)
+
+ursula = new Cat("Ursula", false, "fishies", 9)
