83c9cb4c911261fc5a3380ae2826990bdf84b59f
brewchetta <chett.tiller@gmail.com>
2021-01-13 16:22:21 -0500

    mini lecture 35

--- 35-object-oriented-js-pt-2/index.js
+++ 35-object-oriented-js-pt-2/index.js
@@ -1,9 +1,60 @@
+// Monkey Patching //
+
+Object.prototype.getPrototype = function() {
+  return Object.getPrototypeOf(this)
+}
+
+// Robot Class //
+
 class Robot {
 
   constructor(name, directive, catchphrase) {
-    this._name = name
-    this._directive = directive
+    this.name = name
+    this.directive = directive
     this.catchphrase = catchphrase
   }
 
+  // When reading a name, converts it from binary into a normal string
+  get name() {
+    return binaryToString(this._name)
+  }
+
+  // When writing a name, converts it from a normal string into a binary
+  set name(string) {
+    this._name = stringToBinary(string)
+  }
+
+  // we can use setters and getters even with things that aren't normal attributes
+  get profile() {
+    return `Directive: ${this.directive} // Catchphrase: ${this.catchphrase}`
+  }
+
+}
+
+// Robot Seed Data //
+
+const terminator = new Robot("T-1000", "exterminate all humans", "I'll be back")
+const c3po = new Robot("C3PO", "human cyborg relations", "we're doomed")
+
+// Functions For Converting To And From Binary //
+
+function stringToBinary(input) {
+  var characters = input.split('');
+  return characters.map(char => {
+    const binary = char.charCodeAt(0).toString(2)
+    const pad = Math.max(8 - binary.length, 0);
+    return '0'.repeat(pad) + binary;
+  }).join('');
+}
+
+
+function binaryToString(input) {
+  let bytesLeft = input;
+  let result = '';
+  while (bytesLeft.length) {
+    const byte = bytesLeft.substr(0, 8);
+    bytesLeft = bytesLeft.substr(8);
+    result += String.fromCharCode(parseInt(byte, 2));
+  }
+  return result;
 }
