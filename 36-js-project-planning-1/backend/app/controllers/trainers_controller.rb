307718ed9b415335228cfdf9e8dd271ffc16dd04
brewchetta <chett.tiller@gmail.com>
2021-01-15 09:12:12 -0500

    added controller actions for trainers

--- 36-js-project-planning-1/backend/app/controllers/trainers_controller.rb
+++ 36-js-project-planning-1/backend/app/controllers/trainers_controller.rb
@@ -1,2 +1,11 @@
 class TrainersController < ApplicationController
+  def index
+    trainers = Trainer.all
+    render json: trainers, include: [:pokemons]
+  end
+
+  def show
+    trainer = Trainer.find_by_id(params[:id])
+    render json: trainer, include: [:pokemons]
+  end
 end
