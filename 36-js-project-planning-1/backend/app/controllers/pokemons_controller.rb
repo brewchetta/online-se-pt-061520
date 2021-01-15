079f2515d967cfa8e9c0efe87b6d1cc3587c1fae
brewchetta <chett.tiller@gmail.com>
2021-01-15 09:48:35 -0500

    fixed typo, added specific error status for failed post

--- 36-js-project-planning-1/backend/app/controllers/pokemons_controller.rb
+++ 36-js-project-planning-1/backend/app/controllers/pokemons_controller.rb
@@ -13,11 +13,14 @@ class PokemonsController < ApplicationController
     name = Faker::Name.first_name
     species = Faker::Games::Pokemon.name
     trainer = Trainer.find_by_id(params[:trainer_id])
-    if trainer.pokemons.length < 6 do
+    if trainer && trainer.pokemons.length < 6
       pokemon = Pokemon.create(nickname: name, species: species, trainer: trainer)
       render json: pokemon, include: [:trainer]
     else
-      render json: { error: "Trainer already has 6 pokemons!" }
+      render json: {
+        error: "Trainer must exist and cannot have more than six pokemon!",
+        status: 400
+      }, status: 400
     end
   end
 
