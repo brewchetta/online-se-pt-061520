17057707aa4a90dcd17e7a914d75b0ed2ca46e7b
brewchetta <chett.tiller@gmail.com>
2021-02-08 09:13:30 -0500

    pre-lecture

---
+++ 39-react-class-based-state/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="App">
+
+        <p>Chett is hungry: no</p>
+        <p>Octavia is hungry: yes</p>
+        <p>Ursula is hungry: no</p>
+
+      </div>
+    );
+
+  }
+}
+
+
+export default App;
