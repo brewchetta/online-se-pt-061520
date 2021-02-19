f152c43604044414d2baa667fcc3475a438fa136
brewchetta <chett.tiller@gmail.com>
2021-02-19 09:33:11 -0500

    post lecture

--- 41-react-controlled-forms/src/CalculatorForm.js
+++ 41-react-controlled-forms/src/CalculatorForm.js
@@ -2,19 +2,47 @@ import React from 'react'
 
 class CalculatorForm extends React.Component {
 
-  state = {}
+  state = {
+    valueOne: 1,
+    valueTwo: 1,
+    operator: "add"
+  }
 
   handleSubmit = event => {
     event.preventDefault()
+  }
+
+  handleChange = event => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  handleReset = e => {
+    this.setState({valueOne: 1, valueTwo: 1, operator: "add"})
+  }
+
+  calculateOutcome = () => {
+    const {operator, valueOne, valueTwo} = this.state
+    switch (operator) {
+      case "add":
+        return parseInt(valueOne) + parseInt(valueTwo)
+      case "subtract":
+        return parseInt(valueOne) - parseInt(valueTwo)
+      case "multiply":
+        return parseInt(valueOne) * parseInt(valueTwo)
+      case "divide":
+        return parseInt(valueTwo) !== 0 ? parseInt(valueOne) / parseInt(valueTwo) : "NaN"
+      default:
+        console.error("Something went really wrong!")
+    }
   }
 
   render() {
     return(
       <form id="calculator-form" onSubmit={this.handleSubmit}>
 
-        <input type="number" value={1} />
+        <input name="valueOne" type="number" onChange={this.handleChange} value={this.state.valueOne} />
 
-        <select>
+        <select name="operator" value={this.state.operator} onChange={this.handleChange}>
 
           <option value="add">+</option>
           <option value="subtract">-</option>
@@ -23,9 +51,19 @@ class CalculatorForm extends React.Component {
 
         </select>
 
-        <input type="number" value={1} />
+        <input name="valueTwo" type="number" onChange={this.handleChange} value={this.state.valueTwo} />
 
-        <span> = Some Number</span>
+        <span> = {this.calculateOutcome()}</span>
+
+        <br/>
+
+        <p>ValueOne is: {this.state.valueOne}</p>
+        <p>Value Two is: {this.state.valueTwo}</p>
+        <p>Operator is: {this.state.operator}</p>
+
+        <input type="button"
+        value={"Reset"}
+        onClick={this.handleReset} />
 
       </form>
     )
