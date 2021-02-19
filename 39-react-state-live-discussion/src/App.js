3ea9031566df47f7b92ecda4eabc02e2c66203f9
brewchetta <chett.tiller@gmail.com>
2021-02-18 19:12:46 -0500

    lecture done

--- 39-react-state-live-discussion/src/App.js
+++ 39-react-state-live-discussion/src/App.js
@@ -1,14 +1,28 @@
+import { useState } from "react"
 import GratitudeListItem from './GratitudeListItem.js'
 import './App.css';
 
 function App() {
 
-  const foods = ["pizza", "pasta", "burgers", "sandwiches", "fries", "salads"]
+  const [foods, setFoods] = useState(["pizza", "pasta", "burgers", "sandwiches", "fries", "salads", "ice cream"])
 
-  const renderFoods = () => foods.map(food => <GratitudeListItem item={food} /> )
+  const [darkModeOn, setDarkModeOn] = useState(false)
+
+  const handleClick = () => {
+    setDarkModeOn(!darkModeOn)
+  }
+
+  const removeItem = foodName => {
+    console.log(foodName);
+    setFoods(foods.filter(f => f !== foodName))
+  }
+
+  const renderFoods = () => foods.map(food => <GratitudeListItem key={food} item={food} removeItem={removeItem} /> )
 
   return (
-    <div className="App">
+    <div className="App" style={darkModeOn ? {backgroundColor: 'black', color: "white"} : null}>
+
+      <button onClick={handleClick}>Dark Mode</button>
 
       <h1>Gratitude List</h1>
 
--- 39-react-state-live-discussion/src/GratitudeListItem.js
+++ 39-react-state-live-discussion/src/GratitudeListItem.js
@@ -2,9 +2,33 @@ import React, {Component} from "react"
 
 class GratitudeListItem extends Component {
 
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     gratefulness: 0
+  //   }
+  // }
+
+  state = {
+    gratefulness: 0,
+    color: 'red'
+  }
+
+  handleClick = () => {
+    // this.setState({gratefulness: this.state.gratefulness + 1})
+    //
+    // if (this.state.gratefulness === 4) {
+    //   this.setState({color: 'orange'})
+    // }
+
+    this.props.removeItem(this.props.item)
+  }
+
   render() {
     return (
-      <p>Grateful for {this.props.item}</p>
+      <p>Grateful for {this.props.item}
+        <button style={{backgroundColor: this.state.color}} onClick={this.handleClick}>{this.state.gratefulness}</button>
+      </p>
     )
   }
 
