9a59b8b57269949b04200b7f39794e3e6a86de0e
brewchetta <chett.tiller@gmail.com>
2021-03-05 10:40:28 -0500

    added 45 react router

---
+++ 45-react-router/src/Components/About.js
@@ -0,0 +1,17 @@
+export default function About() {
+
+  return (
+    <>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt quam sed urna ornare, et interdum justo consequat. Maecenas ut risus id orci faucibus fringilla. Nulla sagittis a neque vitae faucibus. Praesent augue lacus, fringilla sit amet molestie at, fringilla ac felis. Duis tincidunt urna a finibus varius. Curabitur metus elit, efficitur quis fermentum at, imperdiet vel ipsum. Morbi ligula diam, mollis a erat ut, finibus dictum ligula.</p>
+
+      <p>Phasellus sollicitudin erat non maximus porttitor. Suspendisse vel tortor ipsum. Nam id tellus sollicitudin, efficitur arcu in, lacinia nibh. Curabitur vel scelerisque quam, non bibendum velit. Suspendisse tristique eros ut ultricies suscipit. Vivamus lacinia est quis turpis bibendum pulvinar. Cras commodo sapien tellus, blandit ultrices magna vestibulum vitae. Vestibulum ut ipsum ac odio mollis vehicula.</p>
+
+      <p>Morbi posuere erat ac auctor lacinia. Nunc ultricies elementum dolor. In condimentum, nulla sit amet efficitur finibus, eros sem porta nulla, vel aliquet lorem libero at nisi. Pellentesque imperdiet lacus in mollis pharetra. Fusce semper varius erat in lobortis. Pellentesque viverra, orci nec malesuada sagittis, turpis felis dignissim nisl, et scelerisque nisi libero at turpis. Duis elementum tincidunt felis.</p>
+
+      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi imperdiet sodales orci quis vulputate. Quisque ac laoreet enim, ac commodo lorem. Aenean faucibus interdum nisi, ut imperdiet ligula bibendum non. Nunc et interdum diam, a sollicitudin risus. Pellentesque nec iaculis turpis. Sed eget sagittis sapien. Nulla nec massa vel velit commodo egestas. Vestibulum cursus nibh et turpis fringilla, molestie posuere tellus placerat. Cras mollis pretium molestie. Duis dictum ipsum eget leo condimentum ultrices.</p>
+
+      <p>Integer sit amet cursus ex, dictum auctor sem. Nunc ultricies sit amet risus et faucibus. Integer pretium ullamcorper quam, non mollis lectus maximus in. Vestibulum turpis justo, rutrum in lacus sit amet, iaculis imperdiet eros. Integer iaculis id est nec vehicula. Nulla ultricies diam felis, vitae finibus nibh suscipit vel. Proin condimentum quis nulla ac sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer laoreet, justo nec semper eleifend, metus purus blandit nibh, a fermentum nibh est sollicitudin augue. Maecenas non enim et odio vulputate sollicitudin. Aenean id lectus in felis finibus imperdiet sit amet ac lacus. Phasellus maximus mi at nulla tristique, pellentesque iaculis urna placerat. Donec consequat eleifend suscipit. Mauris et pulvinar lacus. </p>
+    </>
+  )
+
+}
---
+++ 45-react-router/src/Components/Welcome.js
@@ -0,0 +1,7 @@
+export default function Welcome() {
+
+  return (
+    <p>Welcome To My Awesome Comics Collection!</p>
+  )
+
+}
