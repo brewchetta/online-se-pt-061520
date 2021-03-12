097a95cb50cfc8d88dc1030399547111caee82da
brewchetta <chett.tiller@gmail.com>
2021-03-12 10:33:17 -0500

    fixes for non-working code

--- 46-jwt-auth/backend/config/routes.rb
+++ 46-jwt-auth/backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  namespace :api do
-    namespace :v1 do
+  scope '/api' do
+    scope '/v1' do
       resources :users
       resources :auth, only: [:create]
     end
