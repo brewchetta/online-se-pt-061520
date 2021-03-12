ed16812cda141aca2e3c562d74470d5ca5820dc2
brewchetta <chett.tiller@gmail.com>
2021-03-12 10:15:41 -0500

    lecture part one

---
+++ 46-jwt-auth/backend/app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: @user, jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+end
---
+++ 46-jwt-auth/backend/app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates_presence_of :username
+  validates_uniqueness_of :username
+end
