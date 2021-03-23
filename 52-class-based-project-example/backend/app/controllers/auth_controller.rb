e48400c41eb8da1ab20245b013ab33203cb2e7c0
brewchetta <chett.tiller@gmail.com>
2021-03-23 15:06:46 -0400

    added login route

--- 52-class-based-project-example/backend/app/controllers/auth_controller.rb
+++ 52-class-based-project-example/backend/app/controllers/auth_controller.rb
@@ -1,2 +1,13 @@
 class AuthController < ApplicationController
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      token = encode_token({user_id: user.id})
+      render json: token
+    else
+      render json: {error: "Incorrect username or password!"}, status: :unauthorized
+    end
+  end
+
 end
--- 52-class-based-project-example/backend/config/routes.rb
+++ 52-class-based-project-example/backend/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :grocery_items
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/authenticate', to: 'auth#create'
+
 end
