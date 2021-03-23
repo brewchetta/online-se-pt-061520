86d79adbc44581bc414b9eeebc5217f2df85907b
brewchetta <chett.tiller@gmail.com>
2021-03-23 15:42:20 -0400

    fixed issue with sent token

--- 52-class-based-project-example/backend/app/controllers/auth_controller.rb
+++ 52-class-based-project-example/backend/app/controllers/auth_controller.rb
@@ -5,7 +5,7 @@ class AuthController < ApplicationController
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       token = encode_token({user_id: user.id})
-      render json: token
+      render json: { jwt: token }
     else
       render json: {error: "Incorrect username or password!"}, status: :unauthorized
     end
