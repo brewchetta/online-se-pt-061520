9d414a0564ae7bc7c3989b626633de96597a50b2
brewchetta <chett.tiller@gmail.com>
2021-03-23 16:06:11 -0400

    profile now shows username and stores grocery items

--- 52-class-based-project-example/frontend/src/components/Profile.js
+++ 52-class-based-project-example/frontend/src/components/Profile.js
@@ -3,15 +3,25 @@ import { profileRequest } from '../services/api'
 
 class Profile extends React.Component {
 
+  state = {
+    username: '',
+    groceryItems: []
+  }
+
   componentDidMount() {
-    profileRequest().then(console.log)
+    profileRequest()
+    .then(res => {
+      if (!res.error) {
+        this.setState({username: res.username, groceryItems: res.grocery_items})
+      }
+    })
   }
 
   render() {
     return (
       <div>
 
-        <h1>Your Profile!</h1>
+        <h1>{this.state.username}'s Profile</h1>
 
       </div>
     )
