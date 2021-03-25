890197963e5efcfe4b332c8d496c6bc34c213093
brewchetta <chett.tiller@gmail.com>
2021-03-25 18:55:21 -0400

    getting jwt token also sends user info

--- 52-class-based-project-example/backend/app/controllers/auth_controller.rb
+++ 52-class-based-project-example/backend/app/controllers/auth_controller.rb
@@ -5,7 +5,7 @@ class AuthController < ApplicationController
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       token = encode_token({user_id: user.id})
-      render json: { jwt: token }
+      render json: { jwt: token, user: user }
     else
       render json: {error: "Incorrect username or password!"}, status: :unauthorized
     end
