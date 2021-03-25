c2b24fb72af09a1a6ab68c127cc57f929036bd02
brewchetta <chett.tiller@gmail.com>
2021-03-25 18:27:50 -0400

    added localstorage services and conditional for logout button

--- 52-class-based-project-example/frontend/src/App.js
+++ 52-class-based-project-example/frontend/src/App.js
@@ -6,8 +6,15 @@ import Login from "./components/Login"
 import Profile from "./components/Profile"
 // style
 import './App.css';
+// services
+import { clearToken, getToken } from './services/local-storage'
 
 function App() {
+
+  const handleLogout = () => {
+    clearToken()
+  }
+
   return (
     <Router>
       <div className="App">
@@ -16,11 +23,13 @@ function App() {
 
           <Route exact path="/" render={() => <Home />} />
 
-          <Route path="/login" render={() => <Login />} />
+          <Route path="/login" render={routerProps => <Login {...routerProps} />} />
 
           <Route path="/profile" render={() => <Profile />} />
 
         </Switch>
+
+        {getToken() ? <button onClick={handleLogout}>Logout</button> : null}
 
       </div>
     </Router>
